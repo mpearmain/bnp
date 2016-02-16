3d93ab40175e5906d4e58412e68e4d1b731da3a8
mpearmain <michael.pearmain@gmail.com>
2016-02-16 15:20:29 +0000

    updates to autotune

--- bayesian_optim_runs/xgb_autotune.py
+++ bayesian_optim_runs/xgb_autotune.py
@@ -68,23 +68,23 @@ if __name__ == "__main__":
     y1 = ytrain[ytrain.index.isin(idx1)]
 
     xgboostBO = BayesianOptimization(xgboostcv,
-                                     {'max_depth': (int(6), int(25)),
-                                      'learning_rate': (0.008, 0.02),
-                                      'n_estimators': (int(500), int(2000)),
-                                      'subsample': (0.6, 0.85),
-                                      'colsample_bytree': (0.6, 0.85),
+                                     {'max_depth': (int(6), int(15)),
+                                      'learning_rate': (0.008, 0.015),
+                                      'n_estimators': (int(300), int(1500)),
+                                      'subsample': (0.75, 0.9),
+                                      'colsample_bytree': (0.75, 0.9),
                                       'gamma': (0.00001, 0.01),
                                       'min_child_weight': (int(4), int(15))
                                      })
     # Use last times best as a start point
-    # print("Running previous best 0.443059")
-    # xgboostBO.explore({'colsample_bytree': [0.69999999999999996],
-    #                    'learning_rate': [0.016],
-    #                    'min_child_weight': [25.0],
-    #                    'n_estimators': [534],
-    #                    'subsample': [0.62],
-    #                    'max_depth': [10],
-    #                    'gamma': [0.005]})
+    print("Running previous best 0.443059")
+    xgboostBO.explore({'colsample_bytree': [0.69999999999999996],
+                       'learning_rate': [0.016],
+                       'min_child_weight': [25.0],
+                       'n_estimators': [534],
+                       'subsample': [0.62],
+                       'max_depth': [10],
+                       'gamma': [0.005]})
 
     xgboostBO.maximize(init_points=5, restarts=1000, n_iter=15)
     print('-' * 53)
