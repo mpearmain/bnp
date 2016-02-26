7d93c0212fa1ef9ecf2e852d2b23efa3faf468a9
mpearmain <michael.pearmain@gmail.com>
2016-02-26 09:28:35 +0000

    updates to autotune optimal params Extratrees MP1

--- python/build_meta_extratrees.py
+++ python/build_meta_extratrees.py
@@ -17,16 +17,37 @@ if __name__ == '__main__':
     dataset_version = ["mp1", "kb1", "kb2", "kb3", "kb4", "kb5099", "kb6099"]
     model_type = "etrees"
     # Generate the same random sequences.
-    np.random.seed(42)
-    seed_values = np.random.randint(1, 1e10, len(dataset_version))
+    random_seed = 42
     todate = datetime.datetime.now().strftime("%Y%m%d")
 
+    # construct colnames for the data  - Only alpha numerics as xgboost in
+    # second level metas doesnt like special chars.
+    clfnames = [model_type + str(random_seed) + str(dataset_version[n])
+                for n in range(len(dataset_version))]
 
     # setup model instances
-    clf = [ExtraTreesClassifier(criterion='gini',max_depth=None,n_jobs= -1,random_state= seed_values[1])]
+    clf = [ExtraTreesClassifier(n_jobs= -1, random_state= random_seed, n_estimators= , min_samples_split= , min_samples_leaf=),
+           ExtraTreesClassifier(n_jobs= -1, random_state= random_seed, n_estimators=1665, min_samples_split=2 , min_samples_leaf=2),
+           ExtraTreesClassifier(n_jobs= -1, random_state= random_seed, n_estimators=1784, min_samples_split=5 , min_samples_leaf=3),
+           ExtraTreesClassifier(n_jobs= -1, random_state= random_seed, n_estimators=1258, min_samples_split=4 , min_samples_leaf=2),
+           ExtraTreesClassifier(n_jobs= -1, random_state= random_seed, n_estimators=1972, min_samples_split=3 , min_samples_leaf=2),
+           ExtraTreesClassifier(n_jobs= -1, random_state= random_seed, n_estimators=1577 , min_samples_split=4 , min_samples_leaf=2),
+           ExtraTreesClassifier(n_jobs= -1, random_state= random_seed, n_estimators=1370 , min_samples_split=2 , min_samples_leaf=3)]
 
 
+    # Read xfolds only need the ID and fold 5.
+    print("Reading Cross folds")
+    xfolds = pd.read_csv(projPath + '/input/xfolds.csv', usecols=['ID','fold5'])
 
+    # read the training and test sets
+    print("Reading Train set")
+    train = pd.read_csv(projPath + '/input/train.csv')
+    print("Reading Test set")
+    test = pd.read_csv(projPath + '/input/test.csv')
+
+    # Setup pandas dataframe to store full result in.
+    mvalid = pd.DataFrame(np.nan, index=train.index, columns=clfnames)
+    mfull = pd.DataFrame(np.nan, index=test.index, columns=clfnames)
 
     # Create the loops over the datasets.
     for i, dataset in enumerate(dataset_version):
@@ -42,12 +63,13 @@ if __name__ == '__main__':
         id_test = xtest.ID
         xtest.drop('ID', axis = 1, inplace = True)
 
-        # Read xfolds only need the ID and fold 5.
-        print("Reading Cross folds")
-        xfolds = pd.read_csv(projPath + '/input/xfolds.csv', usecols=['ID','fold5'])
-
-        stacker = BinaryStackingClassifier(base_classifiers=clf, xfolds=xfolds, evaluation=log_loss)
+        stacker = BinaryStackingClassifier(base_classifiers=clf[i],
+                                           xfolds=xfolds,
+                                           evaluation=log_loss,
+                                           colnames=clfnames[i])
         stacker.fit(xtrain, ytrain)
+
+        # Append the results for each dataset back to the master for train and test
 
 
 
