e5bfe043f2a20cdecfe6d171fa2231de07d7cc27
tng-konrad <konrad@Konrads-MBP.fritz.box>
2016-03-07 01:07:25 +0100

    cleanup of lvl2

--- R/build_datasets_lvl2.R
+++ R/build_datasets_lvl2.R
@@ -15,6 +15,15 @@ msg <- function(mmm,...)
   cat(sprintf(paste0("[%s] ",mmm),Sys.time(),...)); cat("\n")
 }
 
+
+# wrapper around logloss preventing Inf/-Inf for 1/0 values
+log_loss <- function(actual, predicted, cutoff = 1e-15)
+{
+  predicted <- pmax(predicted, cutoff)
+  predicted <- pmin(predicted, 1- cutoff)
+  return(logLoss(actual,predicted))
+}
+
 ## data ####
 # list the groups 
 xlist_val <- dir("./metafeatures/", pattern =  "prval", full.names = T)
@@ -24,45 +33,46 @@ xlist_full <- dir("./metafeatures/", pattern = "prfull", full.names = T)
 ii <- 1
 mod_class <- str_split(xlist_val[[ii]], "_")[[1]][[2]]
 xvalid <- read_csv(xlist_val[[ii]])
-xcols <- colnames(xvalid)[1:(ncol(xvalid)-2)]
-xcols <- paste(xcols , ii, sep = "")
-colnames(xvalid)[1:(ncol(xvalid)-2)] <- xcols
+mod_cols <- grep(mod_class, colnames(xvalid))
+colnames(xvalid)[mod_cols] <- paste(mod_class, ii, 1:length(mod_cols), sep = "")
 
 for (ii in 2:length(xlist_val))
 {
   mod_class <- str_split(xlist_val[[ii]], "_")[[1]][[2]]
   xval <- read_csv(xlist_val[[ii]])
-  xcols <- colnames(xval)[1:(ncol(xval)-2)]
-  xcols <- paste(xcols , ii, sep = "")
-  colnames(xval)[1:(ncol(xval)-2)] <- xcols
+  mod_cols <- grep(mod_class, colnames(xval))
+  colnames(xval)[mod_cols] <- paste(mod_class, ii, 1:length(mod_cols), sep = "")
   xvalid <- merge(xvalid, xval)
   msg(ii)
+  print(dim(xvalid))
 }
 
 # aggregate test set
 ii <- 1
 mod_class <- str_split(xlist_full[[ii]], "_")[[1]][[2]]
 xfull <- read_csv(xlist_full[[ii]])
-xcols <- colnames(xfull)[1:(ncol(xfull)-1)]
-xcols <- paste(xcols , ii, sep = "")
-colnames(xfull)[1:(ncol(xfull)-1)] <- xcols
+mod_cols <- grep(mod_class, colnames(xfull))
+colnames(xfull)[mod_cols] <- paste(mod_class, ii, 1:length(mod_cols), sep = "")
 
-for (ii in 2:length(xlist_val))
+for (ii in 2:length(xlist_full))
 {
+  mod_class <- str_split(xlist_full[[ii]], "_")[[1]][[2]]
   xval <- read_csv(xlist_full[[ii]])
-  xcols <- colnames(xval)[1:(ncol(xval)-1)]
-  xcols <- paste(xcols , ii, sep = "")
-  colnames(xval)[1:(ncol(xval)-1)] <- xcols
+  mod_cols <- grep(mod_class, colnames(xval))
+  colnames(xval)[mod_cols] <- paste(mod_class, ii, 1:length(mod_cols), sep = "")
   xfull <- merge(xfull, xval)
   msg(ii)
+  print(dim(xfull))
 }
 
 rm(xval)
 
-# prepare the data
+## process the sets ####
+
+# separate stuff to ensure columns are identical
+id_train <- xvalid$ID; xvalid$ID <- NULL
+id_test <- xfull$ID; xfull$ID <- NULL
 y <- xvalid$target; xvalid$target <- NULL
-id_valid <- xvalid$ID; xvalid$ID <- NULL
-id_full <- xfull$ID; xfull$ID <- NULL
 
 # trim linearly dependent ones 
 print(paste("Pre linear combo trim size ", dim(xvalid)[2]))
@@ -81,38 +91,26 @@ xq1 <- apply(xvalid,1, function(s) quantile(s, 0.1))
 xq2 <- apply(xvalid,1, function(s) quantile(s, 0.25))
 xq3 <- apply(xvalid,1, function(s) quantile(s, 0.75))
 xq4 <- apply(xvalid,1, function(s) quantile(s, 0.9))
-xvalid$xmed <- xMed 
-xvalid$xmax <- xMax 
-xvalid$xmin <- xMin 
-xvalid$xmad <- xMad
-xvalid$xq1 <- xq1 
-xvalid$xq2 <- xq2 
-xvalid$xq3 <- xq3
-xvalid$xq4 <- xq4
+xvalid$xmed <- xMed; xvalid$xmax <- xMax ; xvalid$xmin <- xMin ;xvalid$xmad <- xMad
+xvalid$xq1 <- xq1 ;xvalid$xq2 <- xq2 ;xvalid$xq3 <- xq3; xvalid$xq4 <- xq4
 
+xMed <- apply(xfull,1,median); xMin <- apply(xfull,1,min)
+xMax <- apply(xfull,1,max); xMad <- apply(xfull,1,mad)
 xq1 <- apply(xfull,1, function(s) quantile(s, 0.1))
 xq2 <- apply(xfull,1, function(s) quantile(s, 0.25))
 xq3 <- apply(xfull,1, function(s) quantile(s, 0.75))
 xq4 <- apply(xfull,1, function(s) quantile(s, 0.9))
-xMed <- apply(xfull,1,median); xMin <- apply(xfull,1,min)
-xMax <- apply(xfull,1,max); xMad <- apply(xfull,1,mad)
-xfull$xmed <- xMed 
-xfull$xmax <- xMax 
-xfull$xmin <- xMin 
-xfull$xmad <- xMad
-xfull$xq1 <- xq1 
-xfull$xq2 <- xq2 
-xfull$xq3 <- xq3 
-xfull$xq4 <- xq4
+xfull$xmed <- xMed ;xfull$xmax <- xMax ;xfull$xmin <- xMin ;xfull$xmad <- xMad
+xfull$xq1 <- xq1 ;xfull$xq2 <- xq2 ;xfull$xq3 <- xq3 ;xfull$xq4 <- xq4
 
 rm(xq1, xq2, xq3, xq4, xMad, xMax, xMed, xMin)
 
 
-# To save dataset for quick optimizations
+## save the datasets  ####
 xvalid$target <- y 
-xvalid$ID <- id_valid
+xvalid$ID <- id_train
 write.csv(xvalid, paste('./input/xtrain_lvl2',todate,'.csv', sep = ""), row.names = F)
 
-xfull$ID <- id_full
+xfull$ID <- id_test
 write.csv(xfull, paste('./input/xtest_lvl2',todate,'.csv', sep = ""), row.names = F)
 
