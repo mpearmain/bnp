3df92fdae85b19f70b7f38d22081f57ed2a71e39
tng-konrad <konrad@Konrads-MacBook-Pro.local>
2016-03-07 11:05:35 +0100

    cleanup with naming conventions

--- R/build_datasets_lvl2.R
+++ R/build_datasets_lvl2.R
@@ -40,11 +40,11 @@ for (ii in 2:length(xlist_val))
 {
   mod_class <- str_split(xlist_val[[ii]], "_")[[1]][[2]]
   xval <- read_csv(xlist_val[[ii]])
-  mod_cols <- grep(mod_class, colnames(xval))
-  colnames(xval)[mod_cols] <- paste(mod_class, ii, 1:length(mod_cols), sep = "")
+  mod_cols <- grep(mod_class, colnames(xvalid)) 
+  colnames(xvalid)[mod_cols] <- paste(mod_class, ii, 1:length(mod_cols), sep = "")
   xvalid <- merge(xvalid, xval)
-  msg(ii)
   print(dim(xvalid))
+  msg(ii)
 }
 
 # aggregate test set
@@ -61,11 +61,12 @@ for (ii in 2:length(xlist_full))
   mod_cols <- grep(mod_class, colnames(xval))
   colnames(xval)[mod_cols] <- paste(mod_class, ii, 1:length(mod_cols), sep = "")
   xfull <- merge(xfull, xval)
-  msg(ii)
   print(dim(xfull))
+  msg(ii)
 }
 
 rm(xval)
+
 
 ## process the sets ####
 
