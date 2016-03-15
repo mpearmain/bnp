ab7cb403ad02bd67e57b8e2c6c14bfd51cb164f0
tng-konrad <konrad@Konrads-MBP.fritz.box>
2016-03-15 22:08:39 +0100

    change input dataset

--- python/build_meta_logreg.py
+++ python/build_meta_logreg.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
 
     ## settings
     projPath = os.getcwd()
-    dataset_version = "kb3"
+    dataset_version = "kb5099"
     model_type = "logreg" 
     seed_value = 123
     todate = datetime.datetime.now().strftime("%Y%m%d")
